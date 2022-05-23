128735abc79a0af6b38281fe3e801ba84a09a25c
Antonio Alex <burnoutbohemiann@gmail.com>
2022-05-23 05:25:15 -0300

    Flask setup and contact page

---
+++ src/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/index.html")
+def index():
+    return render_template("index.html")
+
+@app.route("/aboutme.html")
+def aboutme():
+    return render_template("aboutme.html")
+
+@app.route("/projects.html")
+def projects():
+    return render_template("projects.html")
+
+@app.route("/contact.html")
+def contact():
+    return render_template("contact.html")
+    
